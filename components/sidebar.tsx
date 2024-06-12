cc20c5818da613b617393c3321da2081785fa684
judejay <judejay26@gmail.com>
2024-06-12 15:14:44 +0100

    chore: Update sidebar component and package dependencies

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,32 @@
+import {
+    Box,
+    List,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay,
+  } from '@chakra-ui/layout';
+  import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite,
+  } from 'react-icons/md'
 
-
+  import NextImage from 'next/image';
 const Sidebar = () => {
   return (
-    <div >
-      hello from Sidebar
-    </div>
+    <Box  width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="grey">
+      
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+        <NextImage src="/logo.svg" height={60} width={120} alt={''} />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 export default Sidebar;
