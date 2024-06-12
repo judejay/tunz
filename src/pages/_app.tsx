5d5523576d2130e7d4c9aec4d10c9a26c5bc1732
judejay <judejay26@gmail.com>
2024-06-12 10:41:38 +0100

    chore: Update npm dependencies and add Chakra UI and Framer Motion libraries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,52 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "reset-css";
+import { color } from 'framer-motion';
+import { link } from 'fs';
+
+const theme = extendTheme({
+ colors: {
+  gray: {
+    50: '#f9fafb',
+    100: '#f4f5f7',
+    200: '#e5e7eb',
+    300: '#d2d6dc',
+    400: '#9fa6b2',
+    500: '#6b7280',
+    600: '#4b5563',
+    700: '#374151',
+    800: '#1f2937',
+    900: '#111827',
+  },
+},
+components: {
+  Button: {
+    variants: {
+      link: {
+        ":focus": {
+          boxShadow: "none",
+          outline: "none",
+        },
+        ":hover": {
+          textDecoration: "underline",
+        },
+      },
+    },
+
+    baseStyle: {
+      borderRadius: "xl",
+    },
+    sizes: {
+      md: {
+        h: "56px",
+        fontSize: "lg",
+        px: "32px",
+      },
+    },
+  },
+},
+} as any);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
