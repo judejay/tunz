041143369eade58307dacf3efe38de7ca661a7ad
judejay <judejay26@gmail.com>
2024-06-12 17:26:29 +0100

    chore: Update sidebar component and package dependencies

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,29 +1,78 @@
 import {
-    Box,
-    List,
-    ListItem,
-    ListIcon,
-    Divider,
-    Center,
-    LinkBox,
-    LinkOverlay,
-  } from '@chakra-ui/layout';
-  import {
-    MdHome,
-    MdSearch,
-    MdLibraryMusic,
-    MdPlaylistAdd,
-    MdFavorite,
-  } from 'react-icons/md'
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/layout';
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from 'react-icons/md'
+import NextImage from 'next/image';
+import NextLink from 'next/link'; // Add this line to import NextLink
+const menuItems = [
+  {
+    icon: MdHome,
+    label: 'Home',
+    route: '/',
+  },
+  {
+    icon: MdSearch ,
+    label: 'Search',
+    route: '/search',
+  },
+  {
+    icon: MdLibraryMusic ,
+    label: 'Your Library',
+    route: '/library',
+  },
+  {
+    icon: MdPlaylistAdd ,
+    label: 'Create Playlist',
+    route: '/create-playlist',
+  },
+  {
+    icon: MdFavorite ,
+    label: 'Liked Songs',
+    route: '/liked-songs',
+  },
+];
 
-  import NextImage from 'next/image';
 const Sidebar = () => {
   return (
-    <Box  width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="grey">
-      
+    <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="grey">
+
       <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
-        <NextImage src="/logo.svg" height={60} width={120} alt={''} />
+          <NextImage src="/logo.svg" height={60} width={120} alt={''} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={3}>
+            {menuItems.map((menu, index) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.label}>
+              <LinkBox>
+                <NextLink href={menu.route} passHref>
+                  <LinkOverlay>
+                    <ListIcon
+                      as={menu.icon}
+                      color="white"
+                      marginRight="20px"
+                    />
+                    {menu.label}
+                  </LinkOverlay>
+                </NextLink>
+              </LinkBox>
+            </ListItem>
+            ))}
+
+          </List>
         </Box>
       </Box>
     </Box>
