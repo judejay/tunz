fa40b95d94c05e4d39e19ee90000972d29b807fb
judejay <judejay26@gmail.com>
2024-06-12 12:45:55 +0100

    chore: Update PlayerLayout component layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -7,16 +7,17 @@ interface PlayerLayoutProps {
 
 const PlayerLayout = ({ children }: PlayerLayoutProps) => {
   return (
-    <Box width="100vh" height="100vh">
-      <Box position="absolute" top="0" width="250px" left="0">
-        Sidebar
-      </Box>
-
-      <Box marginLeft="250px">{children}</Box>
-      <Box position="absolute" left="0" bottom="0">
-        Player
-      </Box>
+    <Box width="100vw" height="100vh">
+    <Box position="absolute" top="0" width="250px" left="0">
+    Sidebar
     </Box>
+    <Box marginLeft="250px" marginBottom="100px">
+      <Box height="calc(100vh - 100px)">{children}</Box>
+    </Box>
+    <Box position="absolute" left="0" bottom="0">
+      Player
+    </Box>
+  </Box>
   );
 };
 
