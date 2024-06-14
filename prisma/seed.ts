fcc24fcbf8bb08210400226b5a77856982e9925f
judejay <judejay26@gmail.com>
2024-06-14 10:10:20 +0100

    chore: Add duration field to Song model and update package dependencies

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import {  PrismaClient } from '@prisma/client';
+import becryp from 'bcrypt';
+import { artistsData } from './songsData';
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+    await Promise.all(
+        artistsData.map(async (artist) => {
+            return prisma.artist.upsert({
+                where: { name: artist.name },
+                update: {},
+                create: {
+                  name: artist.name,
+                  songs: {
+                    create: artist.songs.map((song) => ({
+                      name: song.name,
+                      title: song.title, 
+                      duration: song.duration,
+                      url: song.url,
+                    })),
+                  },
+                },
+              })
+        }
+    ));
+};
